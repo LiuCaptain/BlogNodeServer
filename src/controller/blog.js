756b89828e4621211edcff9654bf980759901cbe
LiuCaptain <13560280319@163.com>
2024-01-19 00:25:12 +0800

    提交controller blog.js

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,14 +1,14 @@
-const getList = (a, b) => {
+const getList = (author, keyword) => {
   return [
     {
       id: 1,
-      title: '标题',
-      content: '内容'
+      title: '标题1',
+      content: '内容1'
     },
     {
-      id: 1,
-      title: '标题',
-      content: '内容'
+      id: 2,
+      title: '标题2',
+      content: '内容2'
     }
   ];
 };
